a0b99cff7dfab32ec2656c05eb0cf9a1e5a9c3ab
davidccopayapura@gmail.com <davidccopayapura@gmail.com>
2026-05-14 09:42:07 -0500

    02/ 14 de mayo cor del 2026 , 09:42

--- control-backend/src/main.ts
+++ control-backend/src/main.ts
@@ -7,6 +7,11 @@ async function bootstrap() {
 
   app.set('trust proxy', 1);
 
+  app.use((req, res, next) => {
+    console.log("🔥 REQUEST:", req.method, req.url);
+    next();
+  });
+
   app.enableCors({
     origin: 'https://mifront-production.up.railway.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
